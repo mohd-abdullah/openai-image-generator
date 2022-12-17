dfb3f7a0ca34fb33721f584c6d5621d4d8b8f71a
mohdaddullah.it@gmail.com <mohdaddullah.it@gmail.com>
2022-12-17 12:40:45 +0530

    open ai image geration iniitial commit

---
+++ controllers/openaiController.js
@@ -0,0 +1,45 @@
+// https://github.com/openai/openai-node
+
+// https://beta.openai.com/docs/guides/images/usage
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const generateImage = async (req, resp) => {
+    const {prompt, size } = req.body;
+
+    const imageSize = size === 'small'? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+
+    try {
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: imageSize
+        });
+
+        const imageUrl = response.data.data[0].url;
+
+        resp.status(200).json({
+            success: true,
+            data: imageUrl
+        })
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+          } else {
+            console.log(error.message);
+          }
+        resp.status(400).json({
+            success: false,
+            error: 'Image could not be generated'
+        })
+    }
+}
+
+module.exports = { generateImage };
